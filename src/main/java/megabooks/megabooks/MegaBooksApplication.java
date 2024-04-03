f9f6faecaea4bfedb70c6a22e08424532fb46538
김멍몽 <wjdahrrla@gmail.com>
2024-04-03 10:13:13 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/megabooks/megabooks/MegaBooksApplication.java
@@ -0,0 +1,13 @@
+package megabooks.megabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaBooksApplication.class, args);
+    }
+
+}
