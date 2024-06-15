24e893c3434890c39dcc0230d3b466cefd264e92
김멍몽 <wjdahrrla@gmail.com>
2024-06-15 22:43:42 +0900

    refactor: SpringDocOpenApiConfig 주석 추가 - #12

--- src/main/java/megabooks/megabooks/global/common/config/SpringDocOpenApiConfig.java
+++ src/main/java/megabooks/megabooks/global/common/config/SpringDocOpenApiConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 // http://localhost:8080/swagger-ui/index.html#/
 // http://localhost:8080/swagger-ui/index.html#/
+// http://3.38.185.5:8080/swagger-ui/index.html#/
 public class SpringDocOpenApiConfig {
 
     @Bean
