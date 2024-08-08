c88e2607a1140d0c57cc6bfe97104cc26a895894
김멍몽 <wjdahrrla@gmail.com>
2024-08-08 16:41:49 +0900

    refactor: SpringDocOpenApiConfig 리팩토링

--- src/main/java/megabooks/megabooks/global/config/SpringDocOpenApiConfig.java
+++ src/main/java/megabooks/megabooks/global/config/SpringDocOpenApiConfig.java
@@ -4,28 +4,34 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition
+import java.util.Arrays;
+
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "MegaBooks",
+                description = "MegaBooks API 명세서",
+                version = "1.0.0")
+)
 @Configuration
-// http://localhost:8080/swagger-ui/index.html#/
-// http://localhost:8080/swagger-ui/index.html#/
-// http://43.203.239.98:8080/swagger-ui/index.html#/
 public class SpringDocOpenApiConfig {
 
     @Bean
-    public OpenAPI getOpenApi() {
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
-        return new OpenAPI().components(new Components())
-                .info(getInfo());
-
-    }
-
-    private Info getInfo() {
-        return new Info()
-                .version("1.0.0")
-                .description("MegaBooks REST API DOC")
-                .title("MegaBooks Server");
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
     }
 }
+// http://localhost:8080/swagger-ui/index.html#/
+// http://localhost:8080/swagger-ui/index.html#/
+// http://43.203.239.98:8080/swagger-ui/index.html#/
