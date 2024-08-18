d0fb7315d676f89881b1871280385eaac4fa05be
김멍몽 <wjdahrrla@gmail.com>
2024-08-19 00:19:48 +0900

    fix: Firebase 부분 에러 해결

--- src/main/java/megabooks/megabooks/global/firebase/service/FirebaseService.java
+++ src/main/java/megabooks/megabooks/global/firebase/service/FirebaseService.java
@@ -3,6 +3,5 @@ package megabooks.megabooks.global.firebase.service;
 import megabooks.megabooks.global.firebase.dto.FirebaseResponseDTO;
 
 public interface FirebaseService {
-
     FirebaseResponseDTO.FirebaseFindSummaryDetailDTO findDetailSummary(String bookTitle, String summary);
 }
